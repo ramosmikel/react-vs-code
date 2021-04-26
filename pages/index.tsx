6212e22e018a271c2917c86bd8b966f238e50004
Mikel Ramos <ramosmikel@icloud.com>
2021-04-26 11:29:37 +0100

    Setup barebones Editor with theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Editor, { Monaco } from '@monaco-editor/react';
+import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
+import { theme } from '@/lib/utils';
+import s from '@/styles/home.module.css';
 
 const Home = () => {
+  const editorRef = useRef<editor.IStandaloneCodeEditor>(null);
+
+  const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor) => {
+    editorRef.current = editor;
+  };
+  const handleEditorBeforeMount = (monaco: Monaco) => {
+    monaco.editor.defineTheme('monokai', theme.editor.monokai);
+  };
+
   return (
     <>
-      <div className="text-2xl">Example page with TailwindCSS</div>
+      <div className={s.root}>
+        <Editor
+          height="100%"
+          theme="monokai"
+          beforeMount={handleEditorBeforeMount}
+          onMount={handleEditorDidMount}
+        />
+      </div>
     </>
   );
 };
